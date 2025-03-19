5321e2795569d4c23a2ae9e05a622b3c55d9b45e
Dou2ble <22otro@stockholmscience.se>
2025-03-19 10:41:33 +0100

    improved modal

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from "react"
 
 export default function Modal(props: { children: ReactNode }) {
-	return <div className="absolute top-0 left-0 h-full w-full bg-[#00000050] backdrop-blur-sm z-10">
-		{props.children}
+	return <div className="absolute top-0 left-0 h-full w-full bg-[#00000050] backdrop-blur-sm z-10 flex justify-center items-center">
+		<div className="bg-gray-800 text-gray-200 p-4 rounded-xl">
+			{props.children}
+		</div>
 	</div>
 }
