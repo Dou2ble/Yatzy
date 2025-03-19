643be7a4ce5ab71d6eaef3c390b42d83e204afe6
Dou2ble <22otro@stockholmscience.se>
2025-03-19 15:51:30 +0100

    added pointer cursor to button component

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,5 @@
 export default function Button(props: { label: string, disabled: boolean, autofocus: boolean, onClick: () => void }) {
-	return <button className={`bg-blue-400 text-gray-200 font-bold px-4 py-2 rounded-xl text-lg select-none focus:outline-none hover:bg-blue-500 transition-all focus:ring-4 focus:ring-blue-300 ${props.disabled ? "opacity-25" : "opacity-100"}`} onClick={props.onClick} disabled={props.disabled} autoFocus={props.autofocus}>
+	return <button className={`cursor-pointer bg-blue-400 text-gray-200 font-bold px-4 py-2 rounded-xl text-lg select-none focus:outline-none hover:bg-blue-500 transition-all focus:ring-4 focus:ring-blue-300 ${props.disabled ? "opacity-25" : "opacity-100"}`} onClick={props.onClick} disabled={props.disabled} autoFocus={props.autofocus}>
 		{props.label}
 	</button>
 }
