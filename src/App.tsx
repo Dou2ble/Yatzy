6efd93e6268efacc807c0d924ab7c6cd37002a5a
Dou2ble <22otro@stockholmscience.se>
2025-03-21 21:14:04 +0100

    added start menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,169 +1,84 @@
-// import { useState } from 'react'
-import Button from "./Button";
-import CombinationTable from "./CombinationTable";
-import type { DiceData } from "./Dice";
-import Dice from "./Dice";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { newPlayer } from "./player";
-import Modal from "./Modal";
-import _ from "lodash";
-import logo from "./assets/logo.png";
+import Game from "./Game";
+import Button from "./Button";
 import Footer from "./Footer";
-import CheatMenu from "./CheatMenu";
+import _ from "lodash";
 
-const DICE_COUNT = 5;
-const ROLL_COUNT = 3;
+function PlayerNameInput(props: {
+	value: string;
+	onChange: (value: string) => void;
+}) {
+	return (
+		<div className="relative transition-all text-xl border-gray-500 border-2 rounded-md">
+			<input
+				type="text"
+				value={props.value}
+				className="text-center bg-transparent"
+				onChange={(event) => {
+					props.onChange(event.target.value);
+				}}
+			/>
+			<div className="absolute top-0 left-0 h-full flex justify-center items-center pl-1.5">
+				<span className="icon-[mdi--account-edit]"></span>
+			</div>
+		</div>
+	);
+}
 
 export default function App() {
-  const [dice, setDice] = useState(null as null | DiceData[]);
-  const [rolls, setRolls] = useState(ROLL_COUNT);
-  const [players, setPlayers] = useState([newPlayer("Player 1"), newPlayer("Player 2")]);
-	const [currentPlayer, setCurrentPlayer] = useState(0);
-  const [isCheatMenuOpen, setIsCheatMenuOpen] = useState(false);
+	const [players, setPlayers] = useState([newPlayer("Player 1")]);
+	const [isGameStarted, setIsGameStarted] = useState(false);
 
-  function randomDiceValue(): number {
-    return Math.floor(Math.random() * 6 + 1);
-  }
-
-  function rollDice() {
-    if (dice == null) {
-      setDice(
-        Array.from({ length: DICE_COUNT }).map(() => ({
-          value: randomDiceValue(),
-          isLocked: false,
-        })),
-      );
-    } else {
-      const newDice = [...dice];
-      newDice.forEach((dice, i) => {
-        if (!dice.isLocked) {
-          newDice[i].value = randomDiceValue();
-        }
-      });
-      setDice(newDice);
-    }
-
-    setRolls(rolls - 1);
-  }
-
-  function toggleDiceLock(i: number) {
-    if (dice == null) {
-      return;
-    }
-
-    const newDice = [...dice];
-    newDice[i].isLocked = !newDice[i].isLocked;
-    setDice(newDice);
-  }
-
-  function handleKeyDown(event: KeyboardEvent) {
-    if (event.repeat) {
-      return;
-    }
-
-    // control + alt + c
-    if (event.ctrlKey && event.altKey && event.key == "c") {
-      setIsCheatMenuOpen((prevState) => !prevState);
-      console.log("cheat menu toggled");
-    }
-  }
-
-  function nextPlayer() {
-	  if (currentPlayer >= players.length - 1) {
-		  setCurrentPlayer(0)
-	  } else {
-			setCurrentPlayer(currentPlayer + 1);
-	  }
-  }
-
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
-
-    // Cleanup function to remove the event listener
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
-  return (
-    <div className="min-h-lvh h-full bg-gray-800 flex flex-col items-center overflow-x-hidden">
-      <Modal
-        title="Cheat menu"
-        onClose={() => {
-          setIsCheatMenuOpen(false);
-        }}
-        isOpen={isCheatMenuOpen}
-      >
-        <CheatMenu
-          dice={dice}
-          onChange={(newDice) => {
-            setDice(newDice);
-          }}
-        />
-      </Modal>
-
-      <header className="pt-8 pb-4 lg:pt-20 lg:pb-0">
-        <div className="flex justify-center items-center gap-4 text-gray-100 text-8xl font-bold">
-          <img src={logo} alt="logo" className="h-[1em] p-2" /> Yatzy
-        </div>
-        <div className="pt-4">
-          <div className="h-[2px] bg-gray-900 w-full">
-            <div
-              className="bg-primary h-full transition-all"
-              style={{
-                width: `${(100 / 3) * rolls}%`,
-              }}
-            ></div>
-          </div>
-        </div>
-      </header>
-
-      <aside className="lg:absolute lg:left-20 lg:h-lvh lg:flex lg:flex-col lg:justify-center lg:items-center pb-8 pt-3 lg:pb-0 lg:pt-0">
-        <CombinationTable
-          dice={dice}
-          player={players[currentPlayer]}
-          onSubmit={(combinationName: string, combinationScore: number) => {
-            const newPlayers = _.cloneDeep(players);
-            newPlayers[currentPlayer].combinationScoreboard[combinationName] = combinationScore;
-
-            setPlayers(newPlayers);
-            setRolls(ROLL_COUNT);
-            setDice(null);
-						nextPlayer();
-          }}
-        />
-      </aside>
-
-      <main className="lg:flex-1 flex justify-center items-center">
-        <div className="lg:pb-34 flex items-center justify-center flex-col gap-4">
-	        <div className="flex justify-center items-center pb-1">
-		        <div className="flex justify-center items-center gap-2 text-gray-500 transition-all text-xl">
-							<span className="icon-[mdi--account-outline]"></span>
-		  		    {players[currentPlayer].name}
-		        </div>
-          </div>
-
-          <div className="flex gap-2 items-center min-h-16 ">
-            {(dice != null &&
-              dice.map((dice, i) => (
-                <button key={i} onClick={() => toggleDiceLock(i)}>
-                  <Dice value={dice.value} isLocked={dice.isLocked} />
-                </button>
-              ))) || (
-              <strong className="text-gray-200 text-2xl text-center">
-                Please roll the dice
-              </strong>
-            )}
-          </div>
-          <Button
-            label="Roll"
-            onClick={rollDice}
-            disabled={rolls < 1}
-            autofocus
-          />
-        </div>
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="min-h-lvh h-full bg-gray-800 flex flex-col items-center overflow-x-hidden">
+			{isGameStarted ? (
+				<Game
+					players={players}
+					changePlayers={(newPlayers) => {
+						setPlayers(newPlayers);
+					}}
+				/>
+			) : (
+				<div className="flex-1 flex justify-center items-center">
+					<div className="flex flex-col items-center gap-4">
+						<div className="flex flex-col justify-center gap-1.5 items-center pb-1 text-gray-500">
+							{players.map((player, i) => (
+								<PlayerNameInput
+									key={i}
+									value={player.name}
+									onChange={(value) => {
+										const newPlayers = _.cloneDeep(players);
+										newPlayers[i].name = value;
+										setPlayers(newPlayers);
+									}}
+								/>
+							))}
+							<button
+								className="flex justify-center items-center border-gray-500 border-2 rounded-md p-1 transition-all hover:text-gray-300 hover:border-gray-300"
+								onClick={() => {
+									setPlayers((previous) => [
+										...previous,
+										newPlayer(`Player ${previous.length + 1}`),
+									]);
+								}}
+							>
+								<span className="icon-[mdi--account-add]"></span>
+							</button>
+						</div>
+						<strong className="flex justify-center items-center text-gray-200 text-2xl min-h-16">
+							Select players and start the game
+						</strong>
+						<Button
+							label="Start"
+							onClick={() => {
+								setIsGameStarted(true);
+							}}
+						/>
+					</div>
+				</div>
+			)}
+			<Footer></Footer>
+		</div>
+	);
 }
---
+++ src/Game.tsx
@@ -0,0 +1,168 @@
+import Button from "./Button";
+import CombinationTable from "./CombinationTable";
+import type { DiceData } from "./Dice";
+import Dice from "./Dice";
+import { useEffect, useState } from "react";
+import Modal from "./Modal";
+import _ from "lodash";
+import logo from "./assets/logo.png";
+import Footer from "./Footer";
+import CheatMenu from "./CheatMenu";
+import { Player } from "./player";
+
+const DICE_COUNT = 5;
+const ROLL_COUNT = 3;
+
+export default function Game(props: {players: Player[], changePlayers: (players: Player[]) => void}) {
+  const [dice, setDice] = useState(null as null | DiceData[]);
+  const [rolls, setRolls] = useState(ROLL_COUNT);
+  // const [players, setPlayers] = useState([newPlayer("Player 1"), newPlayer("Player 2")]);
+	const [currentPlayer, setCurrentPlayer] = useState(0);
+  const [isCheatMenuOpen, setIsCheatMenuOpen] = useState(false);
+
+  function randomDiceValue(): number {
+    return Math.floor(Math.random() * 6 + 1);
+  }
+
+  function rollDice() {
+    if (dice == null) {
+      setDice(
+        Array.from({ length: DICE_COUNT }).map(() => ({
+          value: randomDiceValue(),
+          isLocked: false,
+        })),
+      );
+    } else {
+      const newDice = [...dice];
+      newDice.forEach((dice, i) => {
+        if (!dice.isLocked) {
+          newDice[i].value = randomDiceValue();
+        }
+      });
+      setDice(newDice);
+    }
+
+    setRolls(rolls - 1);
+  }
+
+  function toggleDiceLock(i: number) {
+    if (dice == null) {
+      return;
+    }
+
+    const newDice = [...dice];
+    newDice[i].isLocked = !newDice[i].isLocked;
+    setDice(newDice);
+  }
+
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.repeat) {
+      return;
+    }
+
+    // control + alt + c
+    if (event.ctrlKey && event.altKey && event.key == "c") {
+      setIsCheatMenuOpen((prevState) => !prevState);
+      console.log("cheat menu toggled");
+    }
+  }
+
+  function nextPlayer() {
+	  if (currentPlayer >= props.players.length - 1) {
+		  setCurrentPlayer(0)
+	  } else {
+			setCurrentPlayer(currentPlayer + 1);
+	  }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  return (
+	  <>
+      <Modal
+        title="Cheat menu"
+        onClose={() => {
+          setIsCheatMenuOpen(false);
+        }}
+        isOpen={isCheatMenuOpen}
+      >
+        <CheatMenu
+          dice={dice}
+          onChange={(newDice) => {
+            setDice(newDice);
+          }}
+        />
+      </Modal>
+
+      <header className="pt-8 pb-4 lg:pt-20 lg:pb-0">
+        <div className="flex justify-center items-center gap-4 text-gray-100 text-8xl font-bold">
+          <img src={logo} alt="logo" className="h-[1em] p-2" /> Yatzy
+        </div>
+        <div className="pt-4">
+          <div className="h-[2px] bg-gray-900 w-full">
+            <div
+              className="bg-primary h-full transition-all"
+              style={{
+                width: `${(100 / 3) * rolls}%`,
+              }}
+            ></div>
+          </div>
+        </div>
+      </header>
+
+      <aside className="lg:absolute lg:left-20 lg:h-lvh lg:flex lg:flex-col lg:justify-center lg:items-center pb-8 pt-3 lg:pb-0 lg:pt-0">
+        <CombinationTable
+          dice={dice}
+          player={props.players[currentPlayer]}
+          onSubmit={(combinationName: string, combinationScore: number) => {
+            const newPlayers = _.cloneDeep(props.players);
+            newPlayers[currentPlayer].combinationScoreboard[combinationName] = combinationScore;
+
+						props.changePlayers(newPlayers);
+            setRolls(ROLL_COUNT);
+            setDice(null);
+						nextPlayer();
+          }}
+        />
+      </aside>
+
+      <main className="lg:flex-1 flex justify-center items-center">
+        <div className="lg:pb-34 flex items-center justify-center flex-col gap-4">
+	        <div className="flex justify-center items-center pb-1">
+		        <div className="flex justify-center items-center gap-2 text-gray-500 transition-all text-xl">
+							<span className="icon-[mdi--account-outline]"></span>
+		  		    {props.players[currentPlayer].name}
+		        </div>
+          </div>
+
+          <div className="flex gap-2 items-center min-h-16 ">
+            {(dice != null &&
+              dice.map((dice, i) => (
+                <button key={i} onClick={() => toggleDiceLock(i)}>
+                  <Dice value={dice.value} isLocked={dice.isLocked} />
+                </button>
+              ))) || (
+              <strong className="text-gray-200 text-2xl text-center">
+                Please roll the dice
+              </strong>
+            )}
+          </div>
+          <Button
+            label="Roll"
+            onClick={rollDice}
+            disabled={rolls < 1}
+            autofocus
+          />
+        </div>
+      </main>
+      <Footer />
+	  </>
+  );
+}
