09ab1daba65a4cd7dbc5fa4165140429e669f20b
Dou2ble <22otro@stockholmscience.se>
2025-03-26 11:15:49 +0100

    added confirmation button to CombinationTable

--- src/CombinationTable.tsx
+++ src/CombinationTable.tsx
@@ -2,13 +2,16 @@ import { combinations } from "./combinations";
 import type { Combination } from "./combinations";
 import { playerTotalScore, type Player } from "./player";
 import type { DiceData } from "./Dice";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import Button from "./Button";
 
 function Row(props: {
   dice: DiceData[] | null;
   player: Player;
   combination: Combination;
-  onSubmit: (combinationName: string, combinationScore: number) => void;
+  isSelected: boolean;
+  // onSubmit: (combinationName: string, combinationScore: number) => void;
+	onSelect: () => void;
 }) {
   const isClickable = useMemo(() => {
     return !(
@@ -19,7 +22,7 @@ function Row(props: {
 
   return (
     <tr
-      className={`relative odd:bg-gray-850 even:bg-gray-900 border-b border-gray-700 transition-all ${isClickable ? "hover:bg-gray-600 hover:text-gray-200 active:bg-gray-500 active:text-gray-100" : ""}`}
+      className={`relative border-b border-gray-700 transition-all ${props.isSelected ? "bg-primary text-gray-200" : "odd:bg-gray-850 even:bg-gray-900"} ${isClickable ? "hover:bg-primary hover:text-gray-200" : ""}`}
     >
       <th
         className={`px-2 text-left ${props.combination.name in props.player.combinationScoreboard ? "line-through decoration-2" : ""}`}
@@ -40,10 +43,7 @@ function Row(props: {
             return;
           }
 
-          props.onSubmit(
-            props.combination.name,
-            props.combination.check(props.dice as DiceData[]),
-          );
+					props.onSelect();
         }}
       ></button>
     </tr>
@@ -55,8 +55,10 @@ export default function CombinationTable(props: {
   player: Player;
   onSubmit: (combinationName: string, combinationScore: number) => void;
 }) {
+	const [selectedRow, setSelectedRow] = useState<null | number>(null);
+
   return (
-    <div className="relative overflow-x-auto rounded-lg">
+    <div className="overflow-x-auto rounded-lg">
       <table className="text-center text-gray-300">
         <thead>
           <tr className="uppercase text-lg bg-gray-700 text-gray-400">
@@ -65,12 +67,13 @@ export default function CombinationTable(props: {
           </tr>
         </thead>
         <tbody>
-          {combinations.map((combination) => (
+          {combinations.map((combination, i) => (
             <Row
               key={combination.name}
               dice={props.dice}
               player={props.player}
-              onSubmit={props.onSubmit}
+              isSelected={i === selectedRow}
+              onSelect={() => {setSelectedRow(i)}}
               combination={combination}
             />
           ))}
@@ -82,6 +85,17 @@ export default function CombinationTable(props: {
           </tr>
         </tfoot>
       </table>
+      <div className="pt-2">
+	      <Button className="w-full" disabled={selectedRow == null || props.dice == null} onClick={() => {
+					if (selectedRow == null || props.dice == null) {
+						return;
+					}
+
+		      const combination = combinations[selectedRow]
+	      	props.onSubmit(combination.name, combination.check(props.dice))
+					setSelectedRow(null);
+	      }}><span className="icon-[mdi--check-bold]"></span></Button>
+      </div>
     </div>
   );
 }
