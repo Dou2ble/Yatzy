059394b84421e78e1368f447c9ee3dd9b712f4f0
Dou2ble <22otro@stockholmscience.se>
2025-03-26 11:10:33 +0100

    added flexbox to Button

--- src/Button.tsx
+++ src/Button.tsx
@@ -7,7 +7,7 @@ export default function Button(props: {
 }) {
   return (
     <button
-      className={`bg-primary text-gray-200 font-bold px-4 py-2 rounded-xl text-lg select-none focus:outline-none transition-all focus:ring-4 focus:ring-primary ${props.className} ${props.disabled ? "opacity-25" : "opacity-100 hover:brightness-80 cursor-pointer"}`}
+      className={`flex justify-center items-center bg-primary text-gray-200 font-bold px-4 py-2 rounded-xl text-lg select-none focus:outline-none transition-all focus:ring-4 focus:ring-primary ${props.className} ${props.disabled ? "opacity-25" : "opacity-100 hover:brightness-80 cursor-pointer"}`}
       onClick={props.onClick}
       disabled={props.disabled}
       autoFocus={props.autofocus}
