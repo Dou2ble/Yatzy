2ac97890ece7ec81b5b41a5aa98b48ea4b6c4fd4
Dou2ble <22otro@stockholmscience.se>
2025-04-25 14:50:25 +0200

    made the header use an h1 tag for better accesibility

--- src/Header.tsx
+++ src/Header.tsx
@@ -23,10 +23,10 @@ function Logo() {
 export default function Header(props: { barWidth: string }) {
 	return (
 		<header className="pt-8 pb-4 lg:pt-20 lg:pb-0">
-			<div className="flex justify-center items-center gap-4 text-gray-100 text-8xl font-bold">
+			<h1 className="flex justify-center items-center gap-4 text-gray-100 text-8xl font-bold">
 				{/* <img src={logo} alt="logo" className="h-[1em] p-2" /> Yatzy */}
 				<Logo /> Yatzy
-			</div>
+			</h1>
 			<div className="pt-4">
 				<div className="h-[2px] bg-gray-900 w-full">
 					<div
